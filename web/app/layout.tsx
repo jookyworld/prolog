5099c5c4ac58209b9ca41a6118a0d2a0a28f7010
jooky <jooky0327@naver.com>
2026-03-28 15:25:09 +0900

    아이콘 변경

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: "ProLog: 상급노하우",
   description:
     "점진적 과부하 추적과 성장 분석으로 꾸준한 운동 습관을 만드세요.",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
